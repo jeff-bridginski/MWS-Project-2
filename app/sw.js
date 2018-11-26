f4cc7766467730f7a7bbf34670c79e20f6511580
Jeff Bridgeman <jeff.bridgeman@outlook.com>
2018-11-25 21:03:36 -0600

    feat: Updates

--- app/sw.js
+++ app/sw.js
@@ -50,34 +50,33 @@ self.addEventListener('install', function(event) {
     );
   });
 
-  self.addEventListener("fetch", event => {
+  self.addEventListener('fetch', event => {
     let cacheRequest = event.request;
 
     let cacheUrlObj = new URL(event.request.url);
     console.log(`URL is ${cacheUrlObj}`);
-    if (event.request.url.indexOf("restaurant.html") > -1) {
-      const cacheURL = "restaurant.html";
+    if (event.request.url.indexOf('restaurant.html') > -1) {
+      const cacheURL = 'restaurant.html';
       cacheRequest = new Request(cacheURL);
     }
 
-    // Requests going to the API get handled separately
     const checkURL = new URL(event.request.url);
-    if (checkURL.port === "1337") {
+    if (checkURL.port === '1337') {
       const parts = checkURL
         .pathname
-        .split("/");
+        .split('/');
       let id = checkURL
         .searchParams
-        .get("restaurant_id") - 0;
+        .get('restaurant_id') - 0;
       if (!id) {
-        if (checkURL.pathname.indexOf("restaurants")) {
-          id = parts[parts.length - 1] === "restaurants"
-            ? "-1"
+        if (checkURL.pathname.indexOf('restaurants')) {
+          id = parts[parts.length - 1] === 'restaurants'
+            ? '-1'
             : parts[parts.length - 1];
         } else {
           id = checkURL
             .searchParams
-            .get("restaurant_id");
+            .get('restaurant_id');
         }
       }
       handleAJAXEvent(event, id);
@@ -87,8 +86,7 @@ self.addEventListener('install', function(event) {
   });
 
   const handleAJAXEvent = (event, id) => {
-    // Only use caching for GET events
-    if (event.request.method !== "GET") {
+    if (event.request.method !== 'GET') {
       return fetch(event.request)
         .then(fetchResponse => fetchResponse.json())
         .then(json => {
@@ -101,21 +99,18 @@ self.addEventListener('install', function(event) {
   }
 
   const handleRestaurantEvent = (event, id) => {
-    // Check the IndexedDB to see if the JSON for the API has already been stored
-    // there. If so, return that. If not, request it from the API, store it, and
-    // then return it back.
     event.respondWith(dbPromise.then(db => {
       return db
-        .transaction("restaurants")
-        .objectStore("restaurants")
+        .transaction('restaurants')
+        .objectStore('restaurants')
         .get(id);
     }).then(data => {
       return (data && data.data) || fetch(event.request)
         .then(fetchResponse => fetchResponse.json())
         .then(json => {
           return dbPromise.then(db => {
-            const tx = db.transaction("restaurants", "readwrite");
-            const store = tx.objectStore("restaurants");
+            const tx = db.transaction('restaurants', 'readwrite');
+            const store = tx.objectStore('restaurants');
             store.put({id: id, data: json});
             return json;
           });
@@ -123,31 +118,25 @@ self.addEventListener('install', function(event) {
     }).then(finalResponse => {
       return new Response(JSON.stringify(finalResponse));
     }).catch(error => {
-      return new Response("Error fetching data", {status: 500});
+      return new Response('Error fetching data', {status: 500});
     }));
   };
 
   const handleNonAJAXEvent = (event, cacheRequest) => {
-    // Check if the HTML request has previously been cached. If so, return the
-    // response from the cache. If not, fetch the request, cache it, and then return
-    // it.
     event.respondWith(caches.match(cacheRequest).then(response => {
       return (response || fetch(event.request).then(fetchResponse => {
         return caches
           .open(cacheID)
           .then(cache => {
-            if (fetchResponse.url.indexOf("browser-sync") === -1) {
+            if (fetchResponse.url.indexOf('browser-sync') === -1) {
               cache.put(event.request, fetchResponse.clone());
             }
             return fetchResponse;
           });
       }).catch(error => {
-        if (event.request.url.indexOf(".jpg") > -1) {
-          return caches.match("/img/na.png");
-        }
-        return new Response("Application is not connected to the internet", {
+        return new Response('Application is not connected to the internet', {
           status: 404,
-          statusText: "Application is not connected to the internet"
+          statusText: 'Application is not connected to the internet'
         });
       }));
     }));
